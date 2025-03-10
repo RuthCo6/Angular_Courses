ffc8b32b3f2ac822e50cbcda3c1f2ddedf7f3068
Ruthy cohen <rc6796580@gmail.com>
2025-03-10 23:51:29 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './../../Courses-Project/src/app/app.component';
+import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
-// import { importProvidersFrom } from '@angular/core';
-// import { AppRoutingModule } from './../../Courses-Project/src/app/app-routing.module';
-// import { BrowserModule } from '@angular/platform-browser';
-// import { HttpClientModule } from '@angular/common/http';
+
 
 bootstrapApplication(AppComponent,appConfig)
 .catch(err => console.error(err));
