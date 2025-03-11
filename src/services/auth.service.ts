f77e15b87472f5a864b6ab5f1392badd3b933016
RuthCo6 <rc6796580@gmail.com>
2025-03-11 21:25:53 +0200

    build-Project

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,88 +1,43 @@
-// import { Injectable } from '@angular/core';
-// import { Observable, tap, catchError } from 'rxjs'; // הוספתי את catchError
-// import { HttpClient } from '@angular/common/http';
-
-// @Injectable({
-//   providedIn: 'root',
-// })
-// export class AuthService {
-//   private baseUrl = 'http://localhost:3000/api/auth/';
-  
-//   constructor(private http: HttpClient) {}
-
-//   login(email: string, password: string): Observable<any> {
-//     const user = { email, password };
-//     return this.http.post(`${this.baseUrl}login`, user).pipe(
-//       tap((response: any) => {
-//         if (response.token) {
-//           sessionStorage.setItem('token', response.token);
-//           localStorage.setItem('userId', response.userId);
-//           console.log(sessionStorage.getItem('token'));
-//           console.log(localStorage.getItem('userId'));
-//         }
-//       }),
-//       catchError((error) => {
-//         console.error('Login error:', error);
-//         throw error; // או אפשר להחזיר Observable.empty() אם רוצים פשוט להתעלם מהשגיאה
-//       })
-//     );
-//   }
-
-//   signUp(name: string, email: string, password: string, role: string): Observable<any> {
-//     const user = { name, email, password, role };
-//     console.log(user);
-//     return this.http.post(`${this.baseUrl}register`, user).pipe(
-//       tap((response: any) => {
-//         if (response.token) {
-//           sessionStorage.setItem('token', response.token);
-//           console.log(sessionStorage.getItem('token'));
-//         }
-//       }),
-//       catchError((error) => {
-//         console.error('SignUp error:', error);
-//         throw error;
-//       })
-//     );
-//   }
-// }
-
-
 import { Injectable } from '@angular/core';
-import {  Observable, tap } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
-
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthService {
-  private baseUrl ='http://localhost:3000/api/auth/';
-  constructor(private http: HttpClient){}
-login(email:string,password:string):Observable<any>
-{
-    const user={email,password}
-return this.http.post(`${this.baseUrl}login`,user).pipe(
-  tap((response:any)=>{
-    if(response.token){
-      sessionStorage.setItem("token",response.token);
-      localStorage.setItem('userId',response.userId)
-      console.log(sessionStorage.getItem("token"));
-      console.log(localStorage.getItem("userId"));     
-    }
-  })
-)
-}
-  signUp(name:string,email:string,password:string,role:string):Observable<any>{
-    const user={name,email,password,role}
-    console.log(user)
-    return this.http.post(`${this.baseUrl}register `,user).pipe(
-      tap((response:any)=>{
-        if(response.token){
-          sessionStorage.setItem("token",response.token)
-          console.log(sessionStorage.getItem("token"));
-          
-        }
-      })
-    )
+export class GetCoursesService {
+  private baseUrl = 'http://localhost:3000/api/courses/';
+  
+  constructor(private http: HttpClient) {}
+
+  deleteCourse(id: number, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.delete(`${this.baseUrl}${id}`, { headers });
+  }
+
+  enrollUserInCourse(courseId: number, userId: string, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.post(`${this.baseUrl}${courseId}/enroll`, { userId }, { headers });
+  }
+
+  unenrollUserFromCourse(courseId: number, userId: string, token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.delete(`${this.baseUrl}${courseId}/unenroll`, {
+      headers,
+      body: { userId }
+    });
+  }
+
+  getAllCourses(token: string): Observable<any> {
+    const headers = new HttpHeaders({
+      'Authorization': `Bearer ${token}`
+    });
+    return this.http.get(this.baseUrl, { headers });
   }
 }
