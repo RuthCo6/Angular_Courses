6153f28bd4b2031b9e1cf05058688106f8c80262
RuthCo6 <rc6796580@gmail.com>
2025-05-07 18:07:58 +0300

    Home and run in render

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from '../components/home/home.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HomeComponent,HomeComponent,RouterModule],
+  imports: [HomeComponent,RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
